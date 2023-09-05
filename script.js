e6b3d14723b3ea33d40848ca32c255ed967d68f7
fbr12 <fabrizzioronco26@gmail.com>
2023-09-05 06:57:01 -0300

    chore:

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let array = [{
     productImg: 'oso.png',
     productName: 'Llave',
@@ -30,4 +31,65 @@ array.forEach((element)=>{
     </div>
   </div>
     `
-})
+})*/
+async function loadJSON() {
+    try {
+      const response = await fetch('archivo.json');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error al cargar el archivo JSON:', error);
+      throw error;
+    }
+  }
+  
+  async function main() {
+    try {
+      const datos = await loadJSON();
+      datos.forEach(element => {
+        if(element.discount==0){
+            const newDiv = document.createElement('div');
+            newDiv.className = 'container'
+            const fDiv = document.querySelector('.featured');
+            fDiv.appendChild(newDiv);
+            newDiv.innerHTML = `
+            <div class="info">
+            <img id="imgf" src="${element.productImg}" alt="">  
+            <h3>${element.productName}</h3>
+            <p>${element.productId}</p>
+        </div>
+        <div class="bottom">
+            <button class="addCart"></button>
+            <div class="prices">
+            <p class="listPrice"></p>
+            <p class="price">${element.productPrice}</p>
+            </div>
+        </div>
+            `}
+        else if(discount!=0){
+            const newDiv = document.createElement('div');
+            newDiv.className = 'container-onsale'
+            const fDiv = document.querySelector('.featured');
+            fDiv.appendChild(newDiv);
+            newDiv.innerHTML = `
+            <div class="info-onsale">
+            <img id="imgf" src="${element.productImg}" alt="">  
+            <h3>${element.productName}</h3>
+            <p>${element.productId}</p>
+          </div>
+          <div class="bottom-onsale">
+            <button class="addCart"></button>
+            <div class="prices-onsale">
+              <p class="listPrice"></p>
+              <p class="price">${element.productPrice}</p>
+            </div>
+          </div>
+            `
+        }
+      });;
+    } catch (error) {
+      console.error('Error en la función principal:', error);
+    }
+  }
+  
+  main();
