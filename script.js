397a32e1dda8e00e40a08ab37ee39062356e16b9
fbr12 <fabrizzioronco26@gmail.com>
2023-09-05 12:49:08 -0300

    fix(script.js):last changes to iterate data.json

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ array.forEach((element)=>{
 })*/
 async function loadJSON() {
     try {
-      const response = await fetch('archivo.json');
+      const response = await fetch('data.json');
       const data = await response.json();
       return data;
     } catch (error) {
@@ -47,44 +47,54 @@ async function loadJSON() {
     try {
       const datos = await loadJSON();
       datos.forEach(element => {
-        if(element.discount==0){
-            const newDiv = document.createElement('div');
-            newDiv.className = 'container'
-            const fDiv = document.querySelector('.featured');
-            fDiv.appendChild(newDiv);
-            newDiv.innerHTML = `
-            <div class="info">
-            <img id="imgf" src="${element.productImg}" alt="">  
-            <h3>${element.productName}</h3>
-            <p>${element.productId}</p>
-        </div>
-        <div class="bottom">
-            <button class="addCart"></button>
-            <div class="prices">
-            <p class="listPrice"></p>
-            <p class="price">${element.productPrice}</p>
-            </div>
-        </div>
-            `}
-        else if(discount!=0){
-            const newDiv = document.createElement('div');
-            newDiv.className = 'container-onsale'
-            const fDiv = document.querySelector('.featured');
-            fDiv.appendChild(newDiv);
-            newDiv.innerHTML = `
-            <div class="info-onsale">
-            <img id="imgf" src="${element.productImg}" alt="">  
-            <h3>${element.productName}</h3>
-            <p>${element.productId}</p>
+        function descuento(){
+          const precio=(element.price-(element.price*element.discount)/100).toFixed(2);
+          return precio;
+        }
+          if(element.discount==0){
+              const newDiv = document.createElement('div');
+              newDiv.className = 'container'
+              const fDiv = document.querySelector('.featured');
+              fDiv.appendChild(newDiv);
+              newDiv.innerHTML = `
+              <div class="info">
+              <img id="imgf" src="${element.img}" alt="">  
+              <h3>${element.name}</h3>
+              <p>${element.id}</p>
           </div>
-          <div class="bottom-onsale">
-            <button class="addCart"></button>
-            <div class="prices-onsale">
+          <div class="bottom">
+              <button class="addCart"></button>
+              <div class="prices">
               <p class="listPrice"></p>
-              <p class="price">${element.productPrice}</p>
-            </div>
+              <p class="price">${element.price}</p>
+              </div>
           </div>
-            `
+              `}
+          else if(element.discount != 0){
+              const precio = descuento();
+              const newDiv = document.createElement('div');
+              newDiv.className = 'container-onsale'
+              const fDiv = document.querySelector('.featured-onsale');
+              fDiv.appendChild(newDiv);
+              newDiv.innerHTML = `
+              <div class="info-onsale">
+                <img src="${element.img}" alt="${element.name}">
+                <svg xmlns="http://www.w3.org/2000/svg" width="101" height="102" viewBox="0 0 101 102" fill="none">
+            <path d="M43.8356 53.4503L47.715 53.524L45.9068 46.6443L43.8356 53.4503Z" fill="#941B0C"/>
+            <path d="M95.4068 46.6657L84.004 34.8229C81.5008 32.2231 76.7125 30.1323 73.1037 30.0637L25.4015 29.1513C23.3627 29.1119 21.4271 29.8729 19.9502 31.2939C18.4732 32.7149 17.6388 34.6196 17.5998 36.6572L17.0664 64.5157C17.0274 66.5533 17.7887 68.4889 19.2098 69.9649C20.6314 71.4413 22.5368 72.2754 24.5756 72.3147L72.2778 73.2264C75.8866 73.2957 80.7514 71.3895 83.3522 68.8873L95.2005 57.4882C98.242 54.5613 98.3345 49.7064 95.4068 46.6657ZM82.0391 53.9897C81.4913 53.9789 80.959 53.7464 80.5789 53.3516C80.1984 52.9564 79.9871 52.4166 79.9976 51.8694C80.0069 51.3211 80.2399 50.7894 80.6352 50.409C81.0301 50.0291 81.5698 49.8175 82.1187 49.8287C82.666 49.8392 83.1973 50.0713 83.5778 50.4664C83.9579 50.8612 84.1703 51.4015 84.1602 51.949C84.1493 52.4965 83.9176 53.0279 83.5226 53.4079C83.1273 53.7882 82.5864 54.0002 82.0391 53.9897Z" fill="#941B0C"/>
+            <text x="25" y="58" class="forSaleText">${element.discount}%</text>
+            </svg>
+                <h3>${element.name}</h3>
+                <p>${element.id}</p>
+              </div>
+              <div class="bottom-onsale">
+                <button class="addCart-onsale"></button>
+                <div class="prices-onsale">
+                  <p class="listPrice">$${element.price}</p>
+                  <p class="price">$${precio}</p>
+                </div>
+              </div>
+              `
         }
       });;
     } catch (error) {
